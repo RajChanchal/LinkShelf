c4e6aecd00097951ebe31480174be0ac8131b70f
Chanchal Raj <rajchancahlkhatri@gmail.com>
2025-12-18 22:36:27 +0400

    Initial Commit

---
+++ LinkShelf/LinkShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkShelfApp.swift
+//  LinkShelf
+//
+//  Created by Chanchal Raj on 18/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct LinkShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
